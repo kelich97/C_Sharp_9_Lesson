e16963e57eaea9f002f23ec975e3f754a10d1aba
Kamalov Keldibek <klbest1797@gmail.com>
2023-07-14 12:35:47 +0300

    66

---
+++ Exercise_66/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int SumOfNums (int n, int m)
+{
+    if(n!=m)
+    {
+        return Math.Min(n,m) + SumOfNums(Math.Min(n,m) + 1 , Math.Max(m,n));
+    }
+    return Math.Min(n,m);
+}
+
+int sum = SumOfNums(1,15);
+Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
